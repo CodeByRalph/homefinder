f0eff9a1ca32c42eff9f925361736fd56fdad4d4
Ralph Stout <ralph.stout14@icloud.com>
2024-01-15 15:15:06 -0600

    Created SearchForm component for homepage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 
+//Component Imports
+import SearchForm from '@/components/SearchForm'
+
 //Image imports
 import MobileImage from "@/public/SmallHero.png";
 import LargeHero from "@/public/LargeHero.png";
@@ -10,19 +13,8 @@ export default function Home() {
       <div>
         <h1 className='text-white p-3 text-lg'>DreamHome</h1>
       </div>
-      <div className='text-center'>
-        <span className='text-lg'>Find your </span><span className='text-xl font-bold'>Dream Home</span>
-        <h1>We can help you find it!</h1>
-      </div>
-      <div className='bg-white opacity-50'>
-         <span>Buy</span><span>Rent</span>
-      </div>
-      <Image 
-        src={MobileImage}
-        alt="Small Hero Image"
-        fill="true"
-        className='z-[-1]'
-      />
+      <SearchForm />
+      <Image src={MobileImage} alt="Small Hero Image" fill="true" className='z-[-1]'/>
     </main>
   )
 }
---
+++ components/SearchForm.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+export default function SearchForm() {
+  return (
+    <div className='mt-24'>
+      <div className='text-center mb-10'>
+        <span className='text-2xl'>Find your </span><span className='text-3xl font-bold'>Dream Home</span>
+        <h1 className='text-xl'>We can help you find it!</h1>
+      </div>
+      <div className='h-auto p-4 backdrop-blur-xs bg-white/30 w-3/4 m-auto border-[1px] border-white rounded-lg'>
+         <div className='flex justify-center mb-3'>
+          <span className='font-bold mr-2 '>Buy</span><span className='font-bold ml-2'>Rent</span>
+         </div>
+         <form className='w-4/5 m-auto flex flex-col items-center gap-2'>
+          <input placeholder='Location' className='text-black w-full pl-3 rounded-lg p-2'/>
+          <input placeholder='Bedrooms' className='text-black w-full pl-3 rounded-lg p-2'/>
+          <button type='submit' className='bg-orange-400 p-2 rounded-md w-2/4 mt-3'>Search</button>
+         </form>
+      </div>
+    </div>
+  )
+}
